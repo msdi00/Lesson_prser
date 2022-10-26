7049872eedb2dc50f172db500bdf4b10bc14dd09
msdi00 <msdi00@mail.ru>
2022-10-26 21:55:08 +0300

    write reference, output data

--- lesson_1/lesson_1.py
+++ lesson_1/lesson_1.py
@@ -1,17 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
-
-headers = {
-    "accept": "*/*",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
-}
+# url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
+#
+# headers = {
+#     "accept": "*/*",
+#     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+# }
 
 # req = requests.get(url)
-with open('url_text', 'r', encoding='utf-8') as file_url:
-    req = file_url.read()
 
+with open('url_text.html', encoding='utf-8') as file_url:
+    src = file_url.read()
 
-src = req
-print(src)
+soup = BeautifulSoup(src, "lxml")
+all_products_href = soup.find_all(class_="mzr-tc-group-item-href")
+for item in all_products_href:
+    print(item)
